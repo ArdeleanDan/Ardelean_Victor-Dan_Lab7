f8d89ce0184279d3e09c694ac9afd9aaa487fd47
ArdeleanDan <victor.ardelean@stud.ubbcluj.ro>
2025-12-03 13:04:05 +0200

    Laborator9

---
+++ ProductPage.xaml.cs
@@ -0,0 +1,49 @@
+using Ardelean_Victor_Dan_Lab7.Models;
+
+namespace Ardelean_Victor_Dan_Lab7;
+
+public partial class ProductPage : ContentPage
+{
+    ShopList sl;
+
+    public ProductPage(ShopList slist)
+    {
+        InitializeComponent();
+        sl = slist;
+    }
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var product = (Product)BindingContext;
+        await App.Database.SaveProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var product = listView.SelectedItem as Product;
+        await App.Database.DeleteProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+    async void OnAddButtonClicked(object sender, EventArgs e)
+    {
+        if (listView.SelectedItem == null)
+            return;
+
+        var p = listView.SelectedItem as Product;
+
+        var lp = new ListProduct()
+        {
+            ShopListID = sl.ID,
+            ProductID = p.ID
+        };
+
+        await App.Database.SaveListProductAsync(lp);
+
+        await Navigation.PopAsync();
+    }
+
+}
---
+++ ValidationBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Ardelean_Victor_Dan_Lab7
+{
+    public class ValidationBehaviour : Behavior<Editor>
+    {
+        protected override void OnAttachedTo(Editor entry)
+        {
+            base.OnAttachedTo(entry);
+            entry.TextChanged += OnEntryTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Editor entry)
+        {
+            base.OnDetachingFrom(entry);
+            entry.TextChanged -= OnEntryTextChanged;
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            ((Editor)sender).BackgroundColor =
+                string.IsNullOrEmpty(args.NewTextValue)
+                ? Color.FromArgb("#AA4A44")
+                : Color.FromArgb("#FFFFFF");
+        }
+    }
+}
